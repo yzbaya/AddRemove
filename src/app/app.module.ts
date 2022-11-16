a395647b8d4e2761f2635a49c10ac63177952efe
yzbaya <101043195+yzbaya@users.noreply.github.com>
2022-11-16 22:13:53 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
-import { NgModule } from '@angular/core';
+ import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodosComponent } from './todos/todos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/models/Todo.ts
@@ -0,0 +1,5 @@
+export class Todo {
+    content: string | undefined;
+    completed: boolean | undefined;
+
+}
